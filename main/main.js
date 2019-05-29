f5f27111c5acaf3ae3dfafa86c7e9026aa1b2495
fann <2211256376@qq.com>
2019-05-29 13:32:45 +0800

    LED灯

--- main/main.js
+++ main/main.js
@@ -1,35 +1,29 @@
-module.exports = function main() {
+module.exports = function main(x) {
     console.log("Debug Info");
-    "use strict"
-var first = ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'];
-var second =['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'];
-var third = ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|'];
+"use strict"
+    var first = ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'];
+    var second =['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'];
+    var third = ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|'];
+    var arr = [];
 
-
-
-function print(arr1,arr2) {
-    let str = '';
-    for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr2.length; j++) {
-            if(arr1[i] == j){
-                str += arr2[j]+' '
+    function print(arr1,arr2) {
+        let str = "";
+        for (let i = 0; i < arr1.length; i++) {
+            for (let j = 0; j < arr2.length; j++) {
+                if(arr1[i] == j){
+                    str += arr2[j]+' '
+                }
             }
         }
+        return str;
     }
-    console.log(str);
-}
 
+    for(let i = 0;i < x.length;i++){
+        arr.push(x.charAt(i));
+    }
 
-
-var num = prompt("请输入要输入数字的个数");
-var arr = new Array();
-
-for (let i = 0; i < num ; i++) {
-    arr[i] = prompt("请输入第"+(i+1)+"个数字:");
-}
-
-console.log("打印的数字如下:")
-print(arr,first)
-print(arr,second)
-print(arr,third)
+    var result = print(arr,first) +'\n'+' '
+    result += print(arr,second) +'\n'+' '
+    result += print(arr,third)
+    return result;
 };
